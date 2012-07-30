c0957f525d4dff6f0f91c62e935e875022b1a961
prabirshrestha <mail@prabir.me>
2012-07-30 18:29:23 -0400

    Initial Rax.Specs

---
+++ src/Rax.Specs/Class1.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Rax
+{
+    // Specification for Rax
+
+    // using Request = IDictionary<string, object>;
+    // using Response = IDictionary<string, object>;
+    // using Next = Action<Exception>;
+    // using MiddleWare = Func<Request, Response, Next, Task>;
+    // using Host = IDictionary<string, object>;
+    // using HostRunner =  System.Action<Host, IEnumerable<Middleware>>;
+
+    using Request = System.Collections.Generic.IDictionary<string, object>;
+    using Response = System.Collections.Generic.IDictionary<string, object>;
+    using Next = System.Action<System.Exception>;
+
+    using Middleware = System.Func<
+        System.Collections.Generic.IDictionary<string, object> /* request */,
+        System.Collections.Generic.IDictionary<string, object> /* response */,
+        System.Action<System.Exception> /* next */,
+        System.Threading.Tasks.Task>;
+
+    using Host = System.Collections.Generic.IDictionary<string, object>;
+  
+    using HostRunner = System.Action<
+        System.Collections.Generic.IDictionary<string, object> /* environment */,
+        System.Collections.Generic.IEnumerable< /* middleware */
+            System.Func<
+                System.Collections.Generic.IDictionary<string, object> /* request */,
+                System.Collections.Generic.IDictionary<string, object> /* response */,
+                System.Action<System.Exception> /* next */,
+                System.Threading.Tasks.Task>
+        >
+    >;
+}
